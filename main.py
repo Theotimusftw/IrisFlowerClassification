4a33953e7ec0b09587de2ad3bbaac296d6871a02
Theo <itaha4250@gmail.com>
2025-04-02 16:16:19 +0300

    refact: reorganised code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from pandas import DataFrame
 from sklearn.preprocessing import LabelEncoder 
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -24,28 +25,33 @@ def split_dataset(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.Data
 
 
 train, validate, test = split_dataset(df)
+train["species"] = le.transform(train["species"])
+test["species"] = le.transform(test["species"])
+validate_check = validate
+test_check = test
+validate.drop(columns="species")
+test.drop(columns="species")
 
 
-train["species"] = le.transform(train["species"]) 
-print(train)
+def scatterplot(df: DataFrame, x: str, y: str, c: str) -> None:
+    plt.scatter(x=df[x], y=df[y], c=df[c])
+    plt.show()
 
-plt.scatter(
-    train["sepal_length"],
-    train["sepal_width"],
-    c=train["species"]
-)
-plt.show()
-plt.scatter(
-    train["petal_length"],
-    train["petal_width"],
-    c=train["species"]
-)
-plt.show()
+
+scatterplot(train, "sepal_length", "sepal_width", "species")
+scatterplot(train, "petal_length", "petal_width", "species")
+
 
 petal_ratio = list(zip(train["petal_length"], train["petal_width"]))
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(petal_ratio, train["species"])
+prediction = knn.predict(list(zip(validate["petal_length"], validate["petal_width"])))
 
-validate.drop(columns="species")
-test.drop(columns="species")
 
+plt.scatter(validate["petal_length"], validate["petal_width"], c=prediction)
+plt.text(
+    x=validate["petal_length"]-1.7,
+    y=validate["petal_width"]-0.7,
+    s=f"something"
+)
+plt.show()
